e13680746e8ba10b38aba0348eecc5c103abf066
wangkangzhang <1368504885@qq.com>
2023-08-30 17:14:22 +0800

    更新

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,46 @@
+export default {
+
+	base: '/yiAnBlog/',// 简单说如下配置就会让你的项目的路径从 `https://domain.com/` 变成 `https://domain.com/base/`
+	lang: 'en-US', // 页面 HTML 的 lang 属性 `<html lang="en-US">`
+	title: '博客模板',
+	description: '博客模板',
+	head: [
+		// 改变title的图标
+		[
+			'link',
+			{
+				rel: 'icon',
+				href: '/img/logo.ico',//图片放在public文件夹下
+			},
+		],
+	],
+	// 主题配置
+	themeConfig: {
+		//   头部导航
+		nav: [
+			{ text: '首页', link: '/' },
+			{ text: '关于', link: '/about/' }
+		],
+		//   侧边导航
+		sidebar: [
+			{ text: '前端基础', link: '/base/' },
+			{
+				text: 'Vue', link: '/base/vue/',
+				items: [
+					{
+						text: '导航',
+						link: '/base/vue/index1',
+						items: [
+							{
+								text: '导航一',
+								link: '/base/vue/index2'
+
+							}
+						]
+					}
+				]
+			},
+			{ text: 'JavaScript', link: '/base/JavaScript/' }
+		]
+	}
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,25 @@
-# 博客模板
+---
+layout: home
+hero:
+  name: 老王技术文档
+  text: 前端学习笔记
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /base/
+    - theme: alt
+      text: 在GitHub上查看
+      link: 'df'
+  image:
+    src: /img/logo.ico
+    alt: 网页logo
+    
+features:
+  - title: 好玩
+    details: Minimal setup with markdown-centered project structure helps you focus on writing.
+  - title: 硬核
+    details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+  - title: 高薪
+    details: VitePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+footer: MIT Licensed | Copyright © 2021-present CoolDream
+---
